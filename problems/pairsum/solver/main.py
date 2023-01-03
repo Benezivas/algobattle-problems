88e312b17a5be5fcbe523c9a7b3f5483837d1512
Imogen <me@imogen.tech>
2023-01-03 19:33:14 +0100

    fix pairsum solver

--- problems/pairsum/solver/main.py
+++ problems/pairsum/solver/main.py
@@ -1,7 +1,7 @@
 """Simple dummy solver for the Pairsum} problem, outputting a static solution."""
 import json
 
-with open("/output/instance/instance.json", "w+") as output:
+with open("/output/solution/solution.json", "w+") as output:
     json.dump({
         "indices": [0, 1, 2, 3],
     }, output)
