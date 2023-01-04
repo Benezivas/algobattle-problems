12ead8952d9de4adf3ba78a7339d4ce7e37f64ba
Imogen <me@imogen.tech>
2023-01-04 01:49:20 +0100

    update pairsum problem with Optimization class

--- problems/pairsum/problem.py
+++ problems/pairsum/problem.py
@@ -3,31 +3,30 @@ import logging
 from typing import ClassVar
 from pydantic import Field
 
-from algobattle.problem import ProblemModel, SolutionModel
+from algobattle.problem import Optimization, OptimiztionSolution
 
 logger = logging.getLogger('algobattle.problems.pairsum')
 
 
-class Pairsum(ProblemModel):
+class Pairsum(Optimization):
     """The pairsum problem."""
 
     name: ClassVar[str] = "Pairsum"
     min_size: ClassVar[int] = 4
 
     numbers: list[int] = Field(min_items=min_size, ge=0, le=2**63-1)
-    solution: "Solution" = Field(hidden=True)
 
     def check_semantics(self, size: int) -> bool:
-        return len(self.numbers) <= size and self.calculate_score(self.solution, size)
+        return len(self.numbers) <= size and super().check_semantics(size)
 
-    def calculate_score(self, solution: "Solution", size: int) -> bool:
-        first = self.numbers[solution.indices[0]] + self.numbers[solution.indices[1]]
-        second = self.numbers[solution.indices[2]] + self.numbers[solution.indices[3]]
-        return first == second
-
-    class Solution(SolutionModel):
+    class Solution(OptimiztionSolution):
         """A solution to a Pairsum problem"""
         indices: list[int] = Field(min_items=4, max_items=4, ge=0)
 
         def check_semantics(self, size: int, instance: "Pairsum") -> bool:
             return all(i < len(instance.numbers) for i in self.indices) and len(self.indices) == len(set(self.indices))
+
+        def score(self, size: int, instance: "Pairsum") -> float:
+            first = instance.numbers[self.indices[0]] + instance.numbers[self.indices[1]]
+            second = instance.numbers[self.indices[2]] + instance.numbers[self.indices[3]]
+            return first == second
