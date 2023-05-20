f31eed077fc60621f2417619f98727fc981693ca
Imogen <me@imogen.tech>
2023-05-20 18:34:11 +0200

    update pathpacking

--- algobattle_problems/pathpacking/problem.py
+++ algobattle_problems/pathpacking/problem.py
@@ -1,70 +1,48 @@
 """The PathPacking problem class."""
-import logging
-
 from typing import ClassVar
 from pydantic import Field
 
-from algobattle.problem import UndirectedGraph, SolutionModel
-
-logger = logging.getLogger('algobattle.problems.pathpacking')
+from algobattle.problem import UndirectedGraph, SolutionModel, ValidationError, Scored
 
 
 class Pathpacking(UndirectedGraph):
-    """The C4subgraphiso problem class."""
+    """The Path Packing problem class."""
 
-    name: ClassVar[str] = "P_2 Path Packing"
+    name: ClassVar[str] = "P_3 Path Packing"
     min_size: ClassVar[int] = 3
 
-    class Solution(SolutionModel):
-        """A solution to a P_2 Path Packing problem."""
+    class Solution(SolutionModel, Scored):
+        """A solution to a Path Packing problem."""
 
         direction: ClassVar = "maximize"
 
         paths: set[tuple[int, int, int]] = Field(ge=0)
 
-        def check_semantics(self, instance: "Pathpacking", size: int) -> bool:
+        def validate_solution(self, instance: "Pathpacking") -> None:
             if not self.all_paths_disjoint(self.paths):
-                return False
-
+                raise ValidationError("Not all paths in the solution are node-disjoint.")
             for path in self.paths:
-                if any(entry >= size for entry in path):
-                    return False
-                if (not self.path_in_instance(path, instance)):
-                    logger.error(f"Path {path} is not part of the instance graph.")
-                    return False
-            return True
+                if any(entry >= instance.num_vertices for entry in path):
+                    raise ValidationError("Solution contains index that is not a valid vertex.")
+                if not self.path_in_instance(path, instance):
+                    raise ValidationError("Solution contains path that is not part of the instance.")
 
-        def all_paths_disjoint(self, paths: set[list[int]]):
+        def all_paths_disjoint(self, paths: set[tuple[int, int, int]]):
             """Check if all paths of the instance are node-disjoint."""
             used_nodes = set()
             for sol_path in paths:
                 for sol_node in sol_path:
                     if sol_node in used_nodes:
-                        logger.error('Not all paths of the solution are node-disjoint!')
                         return False
                     used_nodes.add(sol_node)
             return True
 
-        def path_in_instance(self, path, instance):
-            """Check if a given path is part of the given instance.
-
-            Parameters
-            ----------
-            path : tuple
-                A tuple of the form (i, j, k) which are sequentially connected to form a path.
-            instance : list
-                A set of edges of the form (i, j).
-
-            Returns
-            -------
-            bool
-                True if the path is part of the instance, False otherwise.
-            """
+        def path_in_instance(self, path: tuple[int, int, int], instance: "Pathpacking") -> bool:
+            """Check if a given path is part of the given instance."""
             edge_set = set(instance.edges)
-            if (not ((path[0], path[1]) in edge_set or (path[1], path[0]) in edge_set)
-                    or not ((path[1], path[2]) in edge_set or (path[2], path[1]) in edge_set)):
-                return False
-            return True
+            edge_set.update((v, u) for u, v in edge_set)
+            u, v, w = path
+            return (u, v) in edge_set and (v, w) in edge_set
 
-        def score(self, size: int, instance: "Pathpacking") -> float:
+        def score(self, instance: "Pathpacking") -> float:
             return len(self.paths)
