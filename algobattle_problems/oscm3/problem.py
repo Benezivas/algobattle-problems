f9adc50b7abf8baa27f2fd79568e27d885e1f801
Imogen <me@imogen.tech>
2023-05-20 15:23:22 +0200

    update oscm3

--- algobattle_problems/oscm3/problem.py
+++ algobattle_problems/oscm3/problem.py
@@ -1,12 +1,8 @@
 """The OSCM3 problem class."""
-import logging
 from typing import ClassVar
 from pydantic import Field
-import copy
 
-from algobattle.problem import ProblemModel, SolutionModel
-
-logger = logging.getLogger('algobattle.problems.oscm3')
+from algobattle.problem import ProblemModel, SolutionModel, ValidationError, Scored
 
 
 class OSCM3(ProblemModel):
@@ -15,97 +11,40 @@ class OSCM3(ProblemModel):
     name: ClassVar[str] = "One-Sided Crossing Minimization-3"
     min_size: ClassVar[int] = 1
 
-    adjacent_edges: list[tuple[int, ...]] = Field(ge=0)
+    size: int = Field(ge=0)
+    neighbors: dict[int, set[int]] = Field(ge=0)
 
-    def is_valid(self, size: int) -> bool:
-        return (
-            all(all(i < size for i in entry) for entry in self.adjacent_edges)
-            and all(len(entry) == len(set(entry)) for entry in self.adjacent_edges)
-            and len(self.adjacent_edges) == size
-        )
+    def validate_instance(self, max_size: int) -> None:
+        super().validate_instance(max_size)
+        if self.size > max_size:
+            raise ValidationError("Instance contains too many vertices.")
+        if any(not 0 <= v < self.size for v in self.neighbors):
+            raise ValidationError("Instance contains element of V_1 out of the permitted range.")
+        if any(not 0 <= v < self.size for neighbors in self.neighbors.values() for v in neighbors):
+            raise ValidationError("Instance contains element of V_2 out of the permitted range.")
+        if any(len(neighbors) > 3 for neighbors in self.neighbors.values()):
+            raise ValidationError("A vertex of V_1 has more than 3 neighbors.")
+        for u in range(self.size):
+            self.neighbors.setdefault(u, set())
 
-    class Solution(SolutionModel):
+    class Solution(SolutionModel, Scored):
         """A solution to a One-Sided Crossing Minimization-3 problem"""
 
         direction: ClassVar = "minimize"
 
-        permutation: tuple[int, ...] = Field(ge=0)
+        vertex_order: list[int] = Field(ge=0)
 
-        def is_valid(self, instance: "OSCM3", size: int) -> bool:
-            return (
-                all(i < size for i in self.permutation)
-                and len(self.permutation) == len(set(self.permutation))
-                and len(self.permutation) == size
-            )
+        def validate_solution(self, instance: "OSCM3") -> None:
+            if any(not 0 <= i < instance.size for i in self.vertex_order):
+                raise ValidationError("An element of the solution is not in the permitted range.")
+            if len(self.vertex_order) != len(set(self.vertex_order)):
+                raise ValidationError("The solution contains duplicate numbers.")
+            if len(self.vertex_order) != instance.size:
+                raise ValidationError("The solution does not order the whole instance.")
 
-        def score(self, instance: "OSCM3", size: int) -> float:
-            g = self.Graph(size)
-            i = 0
-            for element in instance.adjacent_edges:
-                g.insert_node(str(i), i, element)
-                i += 1
-            g.reorder_upper_nodes(self.permutation)
-            return g.calculate_number_crossings()
-
-        class Graph:
-            """Helper graph class to calculate solution sizes."""
-
-            def __init__(self, size: int) -> None:
-                self.size = size
-                self.upper_nodes = [None for i in range(size)]
-                self.lower_nodes = [[] for i in range(size)]
-                self.edges = [[] for i in range(size)]
-
-            def insert_node(self, name: str, slot: int, neighbors: tuple) -> None:
-                """Insert a node into a slot of the graph.
-
-                Parameters
-                ----------
-                name : str
-                    Internal name of the node.
-                slot : int
-                    The slot number into which the node is to be inserted into.
-                neighbors : tuple
-                    The tuple of adjacent nodes to the given node.
-                """
-                neighbors = sorted(neighbors)
-                self.upper_nodes[slot] = str(name)
-                i = 0
-                for neighbor in neighbors:
-                    self.lower_nodes[neighbor].append(str(name) + "_" + str(i))
-                    i += 1
-                    self.edges[slot].append(neighbor)
-
-            def calculate_number_crossings(self) -> int:
-                """Calculate and return the number of crossings currently in the graph.
-
-                Returns
-                -------
-                int
-                    The number of crossings in the graph.
-                """
-                crossings = 0
-                for i in range(self.size):
-                    if self.upper_nodes[i]:
-                        for j in range(i + 1, self.size):
-                            if self.upper_nodes[j]:
-                                for lower_node_i in self.edges[i]:
-                                    for lower_node_j in self.edges[j]:
-                                        if lower_node_i > lower_node_j:
-                                            crossings += 1
-                return crossings
-
-            def reorder_upper_nodes(self, permutation: tuple) -> None:
-                """Reorder the nodes currently placed in the slots according to a given permutation.
-
-                Parameters
-                ----------
-                permutation : tuple
-                    A permutation as a tuple of ints.
-                """
-                old_nodes = copy.deepcopy(self.upper_nodes)
-                old_edges = copy.deepcopy(self.edges)
-
-                for i in range(self.size):
-                    self.upper_nodes[i] = old_nodes[permutation[i]]
-                    self.edges[i] = old_edges[permutation[i]]
+        def score(self, instance: "OSCM3") -> float:
+            score = 0
+            for position, vertex in enumerate(self.vertex_order):
+                for i in instance.neighbors[vertex]:
+                    score += sum(j < i for other in self.vertex_order[position:] for j in instance.neighbors[other])
+            return score
