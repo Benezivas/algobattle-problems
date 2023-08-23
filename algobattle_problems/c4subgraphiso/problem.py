199be5272b178ce9fc05849aa7f277cf0b9be226
Imogen <me@imogen.tech>
2023-08-23 02:05:46 +0200

    improve c4subgraph error messaging

--- algobattle_problems/c4subgraphiso/problem.py
+++ algobattle_problems/c4subgraphiso/problem.py
@@ -1,52 +1,41 @@
 """The C4subgraphiso problem class."""
 
+from itertools import combinations, cycle, islice
+from typing import Annotated, Iterator
+from pydantic import field_validator
+
 from algobattle.problem import Problem, SolutionModel, maximize
 from algobattle.util import Role, ValidationError
-from algobattle.types import UndirectedGraph, Vertex
+from algobattle.types import UndirectedGraph, Vertex, UniqueItems
 
+Square = Annotated[tuple[Vertex, Vertex, Vertex, Vertex], UniqueItems()]
+def edges(square: Square) -> Iterator[tuple[Vertex, Vertex]]:
+    """Returns all edges of a square."""
+    return zip(square, islice(cycle(square), 1, None))
+def diagonals(square: Square) -> Iterator[tuple[Vertex, Vertex]]:
+    """Returns the diagonals of a square."""
+    yield square[0], square[2]
+    yield square[1], square[3]
 
 class Solution(SolutionModel[UndirectedGraph]):
     """A solution to a Square Subgraph Isomorphism problem."""
 
-    squares: set[tuple[Vertex, Vertex, Vertex, Vertex]]
+    squares: set[Square]
+
+    @field_validator("squares", mode="after")
+    @classmethod
+    def check_squares(cls, value: set[Square]) -> set[Square]:
+        if any(set(a) & set(b) for a, b in combinations(value, 2)):
+            raise ValueError("A square in the solution is not node-disjoint to at least one other square")
+        return value
 
     def validate_solution(self, instance: UndirectedGraph, role: Role) -> None:
         super().validate_solution(instance, role)
-        self._all_squares_in_instance(instance)
-        self._all_squares_node_disjoint()
-        self._all_squares_induced(instance)
-
-    def _all_squares_node_disjoint(self) -> None:
-        used_nodes = set()
-        for square in self.squares:
-            for node in square:
-                if node in used_nodes:
-                    raise ValidationError("A square in the solution is not node-disjoint to at least one other square.")
-                used_nodes.add(node)
-
-    def _all_squares_induced(self, instance: UndirectedGraph) -> None:
-        edge_set = set(instance.edges)
-        for square in self.squares:
-            # Edges between opposing nodes of a square would mean the square is not induced by its nodes
-            unwanted_edges = [
-                (square[0], square[2]),
-                (square[2], square[0]),
-                (square[1], square[3]),
-                (square[3], square[1]),
-            ]
-            if any(edge in edge_set for edge in unwanted_edges):
-                raise ValidationError("A square in the solution is not induced in the instance.")
-
-    def _all_squares_in_instance(self, instance: UndirectedGraph) -> None:
-        edge_set = set(instance.edges)
-        for square in self.squares:
-            if (
-                not ((square[0], square[1]) in edge_set or (square[1], square[0]) in edge_set)
-                or not ((square[1], square[2]) in edge_set or (square[2], square[1]) in edge_set)
-                or not ((square[2], square[3]) in edge_set or (square[3], square[2]) in edge_set)
-                or not ((square[3], square[0]) in edge_set or (square[0], square[3]) in edge_set)
-            ):
-                raise ValidationError("A square is not part of the instance.")
+        edge_set = set(instance.edges) | {(v, u) for u, v in instance.edges}
+        if any(edge not in edge_set for square in self.squares for edge in edges(square)):
+            raise ValidationError("A square is not part of the instance.")
+        if any(edge in edge_set for square in self.squares for edge in diagonals(square)):
+            raise ValidationError("A square in the solution is not induced in the instance.")
 
     @maximize
     def score(self, instance: UndirectedGraph, role: Role) -> float:
