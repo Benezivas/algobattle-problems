5d97374a225590ebf57b25553e6e6fe3837b16fb
Imogen <me@imogen.tech>
2023-10-06 02:26:30 +0200

    fix tsptimewindows

--- algobattle_problems/tsptimewindows/problem.py
+++ algobattle_problems/tsptimewindows/problem.py
@@ -2,7 +2,7 @@
 from itertools import pairwise
 from math import sqrt
 
-from typing import Annotated, Iterator, Self
+from typing import Annotated, Iterable, Iterator, Self
 from pydantic import Field
 
 from algobattle.problem import Problem, InstanceModel, SolutionModel, minimize
@@ -44,8 +44,7 @@ class Solution(SolutionModel[Instance]):
 
     def location_tour(self, instance: Instance) -> Iterator[Location]:
         """Iterates over all locations in the tour in order, looping back around to the first."""
-        yield from (instance.locations[i] for i in self.tour)
-        yield instance.locations[self.tour[0]]
+        return (instance.locations[i] for i in self.tour)
 
     def validate_solution(self, instance: Instance, role: Role) -> None:
         super().validate_solution(instance, role)
@@ -60,7 +59,12 @@ class Solution(SolutionModel[Instance]):
             if arrival_time > next.max_time:
                 raise ValidationError("The tour visits a location too late.")
             time = max(arrival_time, next.min_time)  # wait until the next location becomes available
-        return time
+        if len(self.tour) >= 2:
+            first, *_, last = self.location_tour(instance)
+            time_to_start = last.distance(first) / speed
+        else:
+            time_to_start = 0
+        return time + time_to_start
 
 
 Tsptimewindows = Problem(
